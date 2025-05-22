2c2b535be274eeea0b8c5a32b343848f3ccbba8a
Crisvargasmz <cristhiancesarvargas12@gmail.com>
2025-05-22 08:29:38 -0600

    Variables de entorno

--- src/assets/database/firebaseconfig.jsx
+++ src/assets/database/firebaseconfig.jsx
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsvhPa3B8EbrWC6MkJdFJ1kxh-nAlQdLs",
-  authDomain: "graficos-fed6b.firebaseapp.com",
-  projectId: "graficos-fed6b",
-  storageBucket: "graficos-fed6b.appspot.com",
-  messagingSenderId: "154951536645",
-  appId: "1:154951536645:web:c26314fc1d694a13148c6c"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
